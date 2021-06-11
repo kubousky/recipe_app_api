c9eee086adfc1c9c9cb063282adfe76d40437a16
Kuba <kparcheta@gmail.com>
2021-06-11 17:22:06 +0200

    create recipe feature

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -47,3 +47,6 @@ class RecipeViewSet(viewsets.ModelViewSet):
             return serializers.RecipeDetailSerializer
 
         return self.serializer_class
+
+    def perform_create(self, serializers):
+        serializers.save(user=self.request.user)
